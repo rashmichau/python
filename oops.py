0a8fe01cf1372020969fae43b3bcec81a3dc787c
rashmichau <161075049+rashmichau@users.noreply.github.com>
2025-02-26 15:11:04 +0530

    oops.py

--- oops.py
+++ oops.py
@@ -1,1 +1,27 @@
-a="
+#Inheritance in python
+#Single level inheritance
+'''class A:
+
+    def feature1(self):
+        print("work with feature1")
+
+    def feature2(self):
+        print("work with feature2")
+class B(A):
+    def feature3(self):
+        print("work with feature3")
+
+a=A()
+b=B()
+b.feature1()'''
+
+# Multilevel inheritance
+
+
+
+
+
+
+
+
+
